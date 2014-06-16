907536b9459529254336f662389459c226e8daa4
Ward Fisher <wfisher@unidata.ucar.edu>
2014-06-16 15:56:37 -0600

    Next: 6.3.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-  has_secure_password  
+ 
   validates :password, presence: true, length: { minimum: 6 }
-
+  has_secure_password  
 end
